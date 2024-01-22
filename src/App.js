ec6ec6feab674d13d00ea3ab6214d537cc4e7053
lexrayne <30938032+lexrayne@users.noreply.github.com>
2024-01-22 02:12:18 -0400

    Enabled file uploading and downloading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
+import { storage } from './firebase'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
+import { v4 } from 'uuid'
 
 function App() {
+  const [fileUpload, setFileUpload] = useState(null)
+  const [fileList, setFileList] = useState([])
+  const fileListRef = ref(storage, 'images/')
+
+  const handleForm = (e) => {
+    e.preventDefault()
+    if (fileUpload === null) return
+
+    const imageRef = ref(storage, `images/${ fileUpload.name + v4()}`)
+    uploadBytes(imageRef, fileUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setFileList((prev) => [...prev, url])
+      })
+    })
+  }
+
+  useEffect(() => {
+    listAll(fileListRef).then((res) => {
+      res.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setFileList((prev) => [...prev, url])
+        })
+      })
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <form onSubmit={handleForm}>
+        <input
+          type='file'
+          onChange={(e) => { setFileUpload(e.target.files[0]) }}
+        />
+        <button>
+          Submit
+        </button>
+      </form>
+      {fileList.map((url) => {
+        return (
+          <>
+            <img src={url} className='image' />
+            <a href={url} download={url}>
+              <button>Download</button>
+            </a>
+          </>
+        )
+      })}
+    </>
+  )
 }
 
-export default App;
+export default App
