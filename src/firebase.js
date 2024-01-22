ddd52a008e3478d4c67f285b525b3f91e56ac789
lexrayne <30938032+lexrayne@users.noreply.github.com>
2024-01-22 02:12:26 -0400

    Renamed file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfnYmvShCaJpm2LJX5Luixjvy_48R0BvU",
+  authDomain: "fire-upl-dwnl.firebaseapp.com",
+  projectId: "fire-upl-dwnl",
+  storageBucket: "fire-upl-dwnl.appspot.com",
+  messagingSenderId: "78983854213",
+  appId: "1:78983854213:web:73a846ef302da0dfc751ec"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
